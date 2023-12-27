320cd9b0bd53a928586a2ea092dfd9b555e214eb
amin ramezani <aminramezanni@gmail.com>
2023-12-28 02:24:48 +0330

    finish the views

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = [
     path("foods/", views.food_list, name="foods"),
+    path("foods/<int:id>/", views.food_detail, name="food-detail"),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- home/views.py
+++ home/views.py
@@ -8,16 +8,40 @@ from .serializers import FoodSerializer
 
 
 @api_view(['GET', 'POST'])
-def food_list(request):
+def food_list(request, format=None):
 
     if request.method == 'GET':
-
         foods = Food.objects.all()
         serilizer = FoodSerializer(foods, many=True)
-        return JsonResponse({'foods': serilizers.data})
+        return Response(serilizer.data)
 
     if request.method == 'POST':
         serilizer = FoodSerializer(data = request.data)
         if serilizer.is_valid():
             serilizer.save()
             return Response(serilizer.data, status=status.HTTP_201_CREATED)
+
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def food_detail(request, id, format=None):
+
+    try:
+        food = Food.objects.get(pk=id)
+    except Food.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = FoodSerializer(food)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = FoodSerializer(food, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        food.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
